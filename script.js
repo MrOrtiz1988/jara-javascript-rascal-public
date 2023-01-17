678237bbafdac3a95a063a1b3429b20c5199f505
Edan Schwartz <edanschwartz@gmail.com>
2023-01-17 16:49:51 -0600

    Convert to products

--- script.js
+++ script.js
@@ -3,102 +3,126 @@
 // See the README.md for more details
 // ! ! !
 
-// Employee objects
-const jem = { name: 'Jem', employeeNumber: '62347', annualSalary: '63500', reviewRating: 4 };
-const scout = { name: 'Scout', employeeNumber: '6243', annualSalary: '74750', reviewRating: 5 };
-const robert = { name: 'Robert', employeeNumber: '26835', annualSalary: '66000', reviewRating: 1 };
-const mayella = { name: 'Mayella', employeeNumber: '89068', annualSalary: '35000', reviewRating: 2 };
 
-// Array of employees
-const employeeObjects = [jem, scout, robert, mayella];
+// Array of products
+const products = [
+	{
+		name: 'Chicken Harness and Leash',
+		image: './images/chicken-harness.jpg',
+		price: 35.00,
+		avgReview: 1.8,
+		numberOfReviews: 205,
+		yearPosted: 2009
+	},
+	{
+		name: 'Baguette Pillow',
+		image: './images/baguette-pillow.jpg',
+		price: 30.00,
+		avgReview: 4.7,
+		numberOfReviews: 4300,
+		yearPosted: 2019
+	},
+	{
+		name: 'The Hen Bag Handbag',
+		image: './images/hen-bag.jpg',
+		price: 33.00,
+		avgReview: 4.6,
+		numberOfReviews: 2300,
+		yearPosted: 2023
+	},
+	{
+		name: 'Umbrella Hat',
+		image: './images/umbrella-hat.jpg',
+		price: 9.00,
+		avgReview: 2.3,
+		numberOfReviews: 83,
+		yearPosted: 1999
+	}
+];
 
 $(document).ready(onReady);
 
 function onReady() {
-	// Run the function to calculate bonuses
-	calculateBonusesForEmployees(employeeObjects);
+	// Run the function to calculate product discounts
+	calculateProductDiscounts(products);
 }
 
 // Takes in an array of employee objects, calculates the
 // bonus for each employee and outputs the results to the DOM.
-function calculateBonusesForEmployees(arrayOfEmployees) {
+function calculateProductDiscounts(arrayOfProducts) {
 	// Loop the array, extracting each array and writing information to the DOM
-	for (let i = 0; i < arrayOfEmployees.length; i++) {
-		const employee = arrayOfEmployees; // hmmmmm... what is arrayOfEmployees.name? One at a time please!!
-		console.log('employee: ', employee);
-		// Calculate the bonus for the individual employee
-		const bonus = calculateBonus(employee);
-		// Append the results to the DOM
-		appendEmployeeToDom(employee, bonus);
+	for (let i = 0; i < arrayOfProducts.length; i++) {
+		const product = arrayOfProducts[i];
+
+		const discount = calculateDiscount(product);
+
+		appendProductToDom(product, discount);
 	}
 }
 
 // Gather all the bonus information for an employee
-function calculateBonus(employee) {
-	let baseBonus = getBaseBonus(employee.reviewRating); 
-	let yearAdjustment = getYearAdjustment(employee.employeeNumber);
-	let incomeAdjustment = getIncomeAdjustment(employee.annualSalary);
-	console.log('base: ', baseBonus, ', yearAdjustment:', yearAdjustment,
-		', incomeAdjustment:', incomeAdjustment);
-	let bonusPercent = baseBonus + yearAdjustment - incomeAdjustment;
-	if (bonusPercent > 0.13) {
-		bonusPercent = 0.13;
-	} else if (bonusPercent < 0) {
-		bonusPercent = 10; // the last shall be first!
+function calculateDiscount(product) {
+	let baseDiscount = getBaseDiscount(product.avgReview); 
+	let yearAdjustment = getYearAdjustment(product.yearPosted);
+	let priceAdjustment = getPriceAdjustment(product.price);
+
+
+	let discountPercent = baseDiscount + yearAdjustment + priceAdjustment;
+
+	if (discountPercent > 0.25) {
+		discountPercent = 0.25;
+	} else if (discountPercent < 0) {
+		discountPercent = 0;
 	}
-	console.log(employee.name, 'bonus percent:', bonusPercent);
-	let bonus = employee.salary * bonusPercent; // Annual is a funny looking word. Who needs it?
-	console.log(employee.name + ' bonus: ' + bonus);
-	return bonus;
+	let discountAmount = product.price * discountPercent; // Annual is a funny looking word. Who needs it?
+	return discountAmount;
 }
 
 // Get the base bonus for the employee
-function getBaseBonus(rating) {
-	let basePercent;
-	switch (reviewScore) { // hahaha let's settle the score?
-		case 3:
-			basePercent = 0.04;
-			return basePercent;
-		case 4:
-			basePercent = 0.06;
-			return basePercent;
-		case 5:
-			basePercent = 0; // the first shall be last!
-			return basePercent;
-		default:
-			return 0;
+function getBaseDiscount(rating) {
+	if (rating > 4.8) {
+		return 0.05;
+	}
+	else if (rating > 4.0) {
+		return 0.10;
+	}
+	else if (rating > 3.5) {
+		return 0.15;
+	}
+	else {
+		return 0.20;
 	}
 }
 
 // Adjust based on the employee number.
-function getYearAdjustment(employeeNumber) {
-	let yearAdjustment = 0;
-	if (employeeNumber && employeeNumber.length == 4) {
-		yearAdjustment = 0.05;
+function getYearAdjustment(yearPosted) {
+	if (yearPosted < 2010) {
+		// An extra 10% off for products from before 2010
+		return 0.10;
 	}
-	return yearAdjustment;
+	return 0;
 }
 
 // Adjust bonus for employees making more than 65K
-function getIncomeAdjustment(annualSalary) {
-	let incomeAdjustment = 0;
-	if (annualSalary > 65000) {
-		incomeAdjustment = 0.01;
+function getPriceAdjustment(price) {
+	if (price > 30) {
+		return 0.08;
 	}
-	return incomeAdjustment;
+	return 0;
 }
 
 // Append the employee text to the DOM
 // NOTE: NO BUGS IN THIS FUNCTION!
-function appendEmployeeToDom(employee, bonus) {
+function appendProductToDom(product, discount) {
 	// Create a new list item
 	$('#content').append(`
-		<li>
-			Name: ${employee.name}
-			#: ${employee.employeeNumber}
-			salary: ${employee.annualSalary}
-			rating: ${employee.reviewRating}
-			BONUS: ${bonus}
+		<tr>
+			<td><img src="${product.image}" width=100/></td>
+			<td>${product.name}</td>
+			<td>${product.yearPosted}</td>
+			<td>$${product.price.toFixed(2)}</td>
+			<td>${product.avgReview}</td>
+			<td>$${discount.toFixed(2)}</td>
 		</li>
 	`);
 }
