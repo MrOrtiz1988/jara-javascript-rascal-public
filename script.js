52b4eca15a64c22577efc781832281eb5631d259
Edan Schwartz <edanschwartz@gmail.com>
2023-01-19 11:20:18 -0600

    🐛🐛🐛🐛🐛

--- script.js
+++ script.js
@@ -10,28 +10,40 @@ const products = [
 		name: 'Chicken Harness and Leash',
 		image: './images/chicken-harness.jpg',
 		price: 35.00,
-		avgReview: 1.8,
+		reviews: {
+			avgRating: 1.8,
+			count: 59281
+		},
 		yearPosted: 2009
 	},
 	{
 		name: 'Baguette Pillow',
 		image: './images/baguette-pillow.jpg',
 		price: 30.00,
-		avgReview: 4.7,
+		reviews: {
+			avgRating: 5,
+			count: 291
+		},
 		yearPosted: 2019
 	},
 	{
 		name: 'The Hen Bag Handbag',
 		image: './images/hen-bag.jpg',
 		price: 33.00,
-		avgReview: 4.6,
+		reviews: {
+			avgRating: 4.6,
+			count: 8032
+		},
 		yearPosted: 2023
 	},
 	{
 		name: 'Umbrella Hat',
 		image: './images/umbrella-hat.jpg',
 		price: 9.00,
-		avgReview: 2.3,
+		reviews: {
+			avgRating: 2.3,
+			count: 6
+		},
 		yearPosted: 1999
 	}
 ];
@@ -48,9 +60,10 @@ function onReady() {
 function calculateProductDiscounts(arrayOfProducts) {
 	// Loop through the array of products
 	for (let i = 0; i < arrayOfProducts.length; i++) {
-		const product = arrayOfProducts[i];
+		// Get a single product object
+		const product = arrayOfProducts;
 
-		// Calculate the discount for this product
+		// Calculate the discount for this one product object
 		const discount = calculateDiscount(product);
 
 		// Render the product and discount to the DOM
@@ -62,10 +75,10 @@ function calculateProductDiscounts(arrayOfProducts) {
 // based on the average review, the year it was posted, and the price
 function calculateDiscount(product) {
 	// Get a discount percentage, based on the product review
-	let reviewDiscount = getReviewDiscount(product.avgReview); 
+	let reviewDiscount = getReviewDiscount(product); 
 	
 	// Get a discount percentage, based on the year the product was posted
-	let yearAdjustment = getYearAdjustment(product.yearPosted);
+	let yearAdjustment = getYearAdjustment(product.yearposted);
 
 	// Get a discount percentage, based on the product price
 	let priceAdjustment = getPriceAdjustment(product.price);
@@ -74,48 +87,59 @@ function calculateDiscount(product) {
 	let discountPercent = reviewDiscount + yearAdjustment + priceAdjustment;
 
 	// The discount cannot be more than 25%, or less that 0%
-	if (discountPercent > 0.25) {
+	if (discountPercent < 0.25) {
 		discountPercent = 0.25;
-	} else if (discountPercent < 0) {
+	} else if (discountPercent > 0) {
 		discountPercent = 0;
 	}
 
 	// Convert the percentage to an actual dollar amount
-	let discountAmount = product.price * discountPercent;
+	let discountAmount = product.price * discount;
 
 	return discountAmount;
 }
 
 // We'll give a bigger discount for lower rated products
-function getReviewDiscount(rating) {
-	if (rating > 4.8) {
-		return 0.05;
+function getReviewDiscount(product) {
+	let discount;
+	if (product.reviews.avgRating = 5) {
+		// perfect rating 🏆, no discount
+		discount = 0;
 	}
-	else if (rating > 4.0) {
-		return 0.10;
+	else if (product.reviews.avgRating > 4.8) {
+		discount = 0.05;
 	}
-	else if (rating > 3.5) {
-		return 0.15;
+	else if (product.reviews.avgRating > 4.0) {
+		discount = 0.10;
+	}
+	else if (product.reviews.avgRating > 3.5) {
+		discount = 0.15;
 	}
 	else {
-		return 0.20;
+		discount = 0.20;
+	}
+
+
+	// Low rating, few reviews, bigger discount
+	if (product.reviews.count < 100) {
+		discount += 0.10;
 	}
 }
 
 // Old products get an extra 10% discount
 function getYearAdjustment(yearPosted) {
 	if (yearPosted < 2010) {
-		return 0.10;
+		return "0.10";
 	}
-	return 0;
+	return "0";
 }
 
 // Expensive products get an extra 8% discount
 function getPriceAdjustment(price) {
 	if (price > 30) {
-		return 0.08;
+		return "0.08";
 	}
-	return 0;
+	return "0";
 }
 
 // Render a <tr> element to the DOM for a product
@@ -127,7 +151,7 @@ function renderProduct(product, discount) {
 			<td>${product.name}</td>
 			<td>${product.yearPosted}</td>
 			<td>$${product.price.toFixed(2)}</td>
-			<td>${product.avgReview}</td>
+			<td>${product.reviews.avgRating}</td>
 			<td>$${discount.toFixed(2)}</td>
 		</li>
 	`);
